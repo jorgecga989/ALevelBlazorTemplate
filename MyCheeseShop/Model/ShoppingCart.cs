172fedea29dd7f01d668d5150b8f2357c9d97428
Jorge Garcia Alcantara <jorge.cga2006@gmail.com>
2024-05-05 15:54:05 +0100

    working on summary page

--- MyCheeseShop/Model/ShoppingCart.cs
+++ MyCheeseShop/Model/ShoppingCart.cs
@@ -10,11 +10,11 @@
             _items = [];
         }
 
-        public void AddItem (Cheese cheese, int quantity)
+        public void AddItem(Cheese cheese, int quantity)
         {
             //add cheese or increase quantity
             var item = _items.FirstOrDefault(item => item.Cheese.Id == cheese.Id);
-            if (item == null) 
+            if (item == null)
             {
                 _items.Add(new CartItem { Cheese = cheese, Quantity = quantity });
             }
@@ -25,28 +25,46 @@
             //cart page
             OnCartUpdated?.Invoke();
         }
+
         public int Count()
         {
             return _items.Count;
             //return num items in the cart
         }
+        public decimal Total()
+        {
+            // sum the price of all items in the cart
+            return _items.Sum(item => item.Cheese.Price * item.Quantity);
+        }
 
-        public IEnumerable<CartItem> GetItems ()
+        public IEnumerable<CartItem> GetItems()
         {
             //copy of items in cart
             return _items;
         }
-        public int GetQuantity(Cheese cheese) 
+
+        public void RemoveItem(Cheese cheese)
+        {
+            _items.RemoveAll(item => item.Cheese.Id == cheese.Id);
+            OnCartUpdated?.Invoke();
+        }
+
+        public void RemoveItem(Cheese cheese, int quantity)
+        {
+            var item = _items.FirstOrDefault(item => item.Cheese.Id == cheese.Id);
+            if (item is not null)
+            {
+                item.Quantity -= quantity;
+                if (item.Quantity <= 0)
+                    _items.Remove(item);
+            }
+            OnCartUpdated?.Invoke();
+        }
+
+        public int GetQuantity(Cheese cheese)
         {
             var item = _items.FirstOrDefault(item => item.Cheese.Id == cheese.Id);
             return item?.Quantity ?? 0;
         }
-
-        public IEnumerable<CartItem> GetItem()
-        {
-            //return a copy of the items in the cart
-            return _items;
-        }
-
     }
 }
