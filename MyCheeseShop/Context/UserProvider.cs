b6663214ff280ef056e73be2702168bfabd61862
jorgecga989 <jorge.cga2006@gmail.com>
2024-05-10 10:34:17 +0100

    working on checkout display

--- MyCheeseShop/Context/UserProvider.cs
+++ MyCheeseShop/Context/UserProvider.cs
@@ -7,9 +7,16 @@ namespace MyCheeseShop.Context
     public class UserProvider
     {
         private readonly DatabaseContext _context;
+        private readonly UserManager<User> _userManager;
 
+        public UserProvider(DatabaseContext context, UserManager<User> userManager)
+        {
+            _context = context;
+            _userManager = userManager;
+        }
         public User? GetUserByUsername(string? username)
         {
+            // Return the user with the specified username
             return _context.Users.FirstOrDefault(user => user.UserName == username);
         }
     }
